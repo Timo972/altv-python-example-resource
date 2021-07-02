229f15061fab31739953e62bf72ef11b67788c9b
Timo972 <44401485+Timo972@users.noreply.github.com>
2021-07-03 00:46:56 +0200

    files

---
+++ main.py
@@ -0,0 +1,24 @@
+import alt
+
+alt.log("Hello World!")
+
+
+def playerConnect(player):
+    player.model = "mp_m_freemode_01"
+    player.spawn(5, 5, 76, 0)
+    veh = alt.Vehicle("krieger", 5, 5, 76, 0, 0, 0)
+    alt.log("Created vehicle")
+    print(veh)
+    alt.log("Vehicle ID: {}".format(veh.id))
+
+def playerEnterVehicle(player, vehicle, seat):
+    alt.log("Player {0} entered vehicle {1}".format(player.name, vehicle.model))
+    vehicle.customPrimaryColor = alt.RGBA(243, 255, 0, 255)
+
+def playerLeftVehicle(player, vehicle, seat):
+    alt.log("Player {0} left vehicle {1}".format(player.name, vehicle.model))
+    vehicle.customPrimaryColor = alt.RGBA(0, 0, 0, 255)
+
+alt.on("playerConnect", playerConnect)
+alt.on("playerEnterVehicle", playerEnterVehicle)
+alt.on("playerLeftVehicle", playerLeftVehicle)
